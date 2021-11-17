b70c0ecf6ea46c2100a83e07191bab8261001e49
Takipipo <ktphap.ncu@gmail.com>
2021-11-17 16:54:42 +0700

    queue_B was created toreceive a message from worker_A

--- src/worker_A.py
+++ src/worker_A.py
@@ -5,11 +5,12 @@ def main():
     channel = connection.channel()
 
     channel.queue_declare(queue='queue_A')
-    # channel.queue_declare(queue='queue_B')
+    channel.queue_declare(queue='queue_B')
 
 
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+        ch.basic_publish(exchange='', routing_key='queue_B', body=body)
+        print(f'Message received: {body.decode("utf-8")}')
 
     channel.basic_consume(queue='queue_A', on_message_callback=callback, auto_ack=True)
 

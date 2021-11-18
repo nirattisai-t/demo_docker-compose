1ccefaf0b3c586b8c3c0ed18d867c6417a1abf46
Takipipo <ktphap.ncu@gmail.com>
2021-11-18 16:59:39 +0700

    Establish mongodb connection with the fetch message feature to the db

--- src/worker_B.py
+++ src/worker_B.py
@@ -1,20 +1,26 @@
 import pika, sys, os
-import pymongo
+from pymongo import MongoClient
 def main():
-    host = '172.17.0.1'
-    port = '5672'
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port))
-    channel = connection.channel()
-    
-
-    channel.queue_declare(queue='queue_B')
 
     def callback(ch, method, properties, body):
-        print(f'Message received: {body.decode("utf-8")}',flush=True)
+        body = body.decode("utf-8")
+        print(f'Message received: {body}',flush=True)
 
         # worker_B publish the message to mongodb
+        post = {"message": body}
+        
+        collection.insert_one(post)
 
- 
+
+    # Mongodb connection
+    client = MongoClient(host="172.17.0.1",port=27017)
+    collection = client['test_db']['test_collection']
+
+    # RabbitMQ connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.1'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='queue_B')
 
     channel.basic_consume(queue='queue_B', on_message_callback=callback, auto_ack=True)
 

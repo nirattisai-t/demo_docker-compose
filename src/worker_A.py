63c55157f5f3877143f5c7a48530b0854bd548d9
Takipipo <ktphap.ncu@gmail.com>
2021-11-23 13:47:26 +0700

    Use black format to format worker_A and worker_B

--- src/worker_A.py
+++ src/worker_A.py
@@ -1,21 +1,23 @@
 import pika, sys, os, json
 
+
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbit"))
     channel = connection.channel()
 
     channel.exchange_declare(exchange="dlx", exchange_type="direct")
 
-    channel.queue_declare(queue='queue_A', arguments={
-        "x-dead-letter-exchange": "dlx",
-        "x-dead-letter-routing-key": "dlx_key"
-    })
-    channel.queue_declare(queue='queue_B')
+    channel.queue_declare(
+        queue="queue_A",
+        arguments={
+            "x-dead-letter-exchange": "dlx",
+            "x-dead-letter-routing-key": "dlx_key",
+        },
+    )
+    channel.queue_declare(queue="queue_B")
 
     channel.queue_declare(queue="dl_queue")
     channel.queue_bind(queue="dl_queue", exchange="dlx", routing_key="dlx_key")
-
-
 
     def callback(ch, method, properties, body):
         has_error = False
@@ -24,47 +26,55 @@ def main():
         # Assume 2 operands, if operator = "-" --> x1 - x2
         try:
             body_dict = json.loads(body.decode("utf-8"))
-            print(f'Message received: {body}', flush=True)
+            print(f"Message received: {body}", flush=True)
 
-            if body_dict["operator"] == "+": 
-                body_dict["result"] = body_dict["operands"][0] + body_dict["operands"][1]
-            
-            elif body_dict["operator"] == "-": 
-                body_dict["result"] = body_dict["operands"][0] - body_dict["operands"][1]
-            
-            elif body_dict["operator"] == "*": 
-                body_dict["result"] = body_dict["operands"][0] * body_dict["operands"][1]
-            
-            elif body_dict["operator"] == "/": 
-                body_dict["result"] = body_dict["operands"][0] / body_dict["operands"][1]
-            
+            if body_dict["operator"] == "+":
+                body_dict["result"] = (
+                    body_dict["operands"][0] + body_dict["operands"][1]
+                )
+
+            elif body_dict["operator"] == "-":
+                body_dict["result"] = (
+                    body_dict["operands"][0] - body_dict["operands"][1]
+                )
+
+            elif body_dict["operator"] == "*":
+                body_dict["result"] = (
+                    body_dict["operands"][0] * body_dict["operands"][1]
+                )
+
+            elif body_dict["operator"] == "/":
+                body_dict["result"] = (
+                    body_dict["operands"][0] / body_dict["operands"][1]
+                )
+
             # else:
             # add exception scenario
             else:
                 has_error = True
 
-            if has_error:    
-                ch.basic_nack(delivery_tag = method.delivery_tag,requeue = False)
+            if has_error:
+                ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+
             else:
-                ch.basic_ack(delivery_tag = method.delivery_tag)
+                ch.basic_ack(delivery_tag=method.delivery_tag)
                 result_json = json.dumps(body_dict)
 
-                ch.basic_publish(exchange='', routing_key='queue_B', body=result_json)
+                ch.basic_publish(exchange="", routing_key="queue_B", body=result_json)
         except:
-                print("except", flush=True)
-                ch.basic_nack(delivery_tag = method.delivery_tag ,requeue = False)
+            print("except", flush=True)
+            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
 
-    
-
-    channel.basic_consume(queue='queue_A', on_message_callback=callback, auto_ack=False)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.basic_consume(queue="queue_A", on_message_callback=callback, auto_ack=False)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
--- src/worker_B.py
+++ src/worker_B.py
@@ -2,39 +2,40 @@ import pika, sys, os
 from pymongo import MongoClient
 import json
 
-def main():
 
+def main():
     def callback(ch, method, properties, body):
         body = body.decode("utf-8")
         body = json.loads(body)
-        print(f'Message received: {body}',flush=True)
+        print(f"Message received: {body}", flush=True)
 
-        # worker_B publish the message to mongodb        
+        # worker_B publish the message to mongodb
         collection.insert_one(body)
 
     ## add logic to do the operant
     ## remove gateway connection and use service name instead
-    
+
     # Mongodb connection
     client = MongoClient(host="mongodb")
-    collection = client['test_db']['test_collection']
+    collection = client["test_db"]["test_collection"]
 
     # RabbitMQ connection
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbit'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbit"))
     channel = connection.channel()
 
-    channel.queue_declare(queue='queue_B')
+    channel.queue_declare(queue="queue_B")
 
-    channel.basic_consume(queue='queue_B', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue="queue_B", on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages from worker_A. To exit press CTRL+C')
+    print(" [*] Waiting for messages from worker_A. To exit press CTRL+C")
     channel.start_consuming()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
